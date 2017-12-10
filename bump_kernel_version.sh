879389bb581764a092dd139c2d9152d75c1280c1
Georg Drees <tuxlifan@derseegrog.org>
2017-12-10 13:18:05 +0100

    Helper script to bump Linux kernel versions easily

---
+++ bump_kernel_version.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+#
+# Dependencies:
+#   bash, echo, git, grep, sed, tail, wc
+#   sort with --version-sort (-V)
+#
+
+MAKEFILE=Makefile.config
+
+if [ "$(git diff --exit-code --staged)" ]; then
+    echo "Something is staged already. Bailing out!"
+    exit 1
+fi
+if [ "$(git diff --exit-code ${MAKEFILE})" ]; then
+    echo "${MAKEFILE} has been modified. Bailing out!"
+    exit 1
+fi
+if [ "x$(git symbolic-ref --short HEAD)" != "xmaster" ]; then
+    echo "Not on master branch. Bailing out!"
+    exit 1
+fi
+
+read -p "Please enter the new version number: " VERSION
+LINUX_VER=$(grep LINUX_VER ${MAKEFILE}|cut -d= -f2)
+
+if [ "x${VERSION}" == "x${LINUX_VER}" ]; then
+    echo "Version number already in ${MAKEFILE}. Nothing to do."
+    exit 0
+fi
+if [ "x${VERSION}" != "x$(echo -e "${VERSION}\n${LINUX_VER}"|sort --version-sort|tail -n1)" ]; then
+    echo "Your version number is not newer. Bailing out!"
+    exit 1
+fi
+
+sed -i -e '/^LINUX_VER=/s#=.*$#='${VERSION}'#' ${MAKEFILE}
+git add ${MAKEFILE}
+
+if [ 1 -neq $(git diff --staged|grep '^+[^+]'|wc -l) ]; then
+    echo "Something went wrong with 'sed' and 'git add'. More than one line changed! Bailing out!"
+    exit 1
+fi
+
+git commit -m "Bump kernel version to ${VERSION}" -o ${MAKEFILE}
+
+echo "All done. Now you can 'make linux'"
